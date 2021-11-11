3841ec1c8c5a73c7810e5017a52642f28dbb1fd9
Mattias Fält <mattias.falt@umu.se>
2021-11-11 12:51:07 +0100

    La till interface på WebformsMVPDemoEntities (DbContext )

---
+++ src/Demos.WebformsMVP.DataAccess/ExtendedDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace Demos.WebformsMVP.DataAccess
+{
+    public partial class WebformsMVPDemoEntities : IDbContext
+    {
+        public WebformsMVPDemoEntities(string connectionString) : base(connectionString) { }
+
+        public new virtual IDbSet<T> Set<T>() where T : class
+        {
+            return base.Set<T>();
+        }
+    }
+}
---
+++ src/Demos.WebformsMVP.DataAccess/IDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Demos.WebformsMVP.DataAccess
+{
+    public interface IDbContext : IDisposable
+	{
+		IDbSet<T> Set<T>() where T : class;
+
+		int SaveChanges();
+	}
+}
