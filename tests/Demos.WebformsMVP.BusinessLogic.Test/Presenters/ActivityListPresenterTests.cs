8ef3d1936a9f0082f95ab9eb3dc3c1b4ad2d8c98
Mattias Fält <mattias.falt@knowit.se>
2021-11-28 21:42:31 +0100

    Mer tester

--- tests/Demos.WebformsMVP.BusinessLogic.Test/Presenters/ActivityListPresenterTests.cs
+++ tests/Demos.WebformsMVP.BusinessLogic.Test/Presenters/ActivityListPresenterTests.cs
@@ -1,7 +1,9 @@
 ﻿using Demos.WebformsMVP.BusinessLogic.Interfaces;
 using Demos.WebformsMVP.BusinessLogic.Presenters;
 using Moq;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -17,50 +19,110 @@ namespace Demos.WebformsMVP.BusinessLogic.Test.Presenters
         }
 
         [Fact]
-        public void SampleTest()
+        public void NotLoggedInUserShouldBeRedirectedToLoginView()
         {
             //Arrange
+            var userMock = new Mock<IUserInfo>();
+            userMock.Setup(user => user.ID).Returns(0);
             var viewMock = new ActivityListViewMock();
+            viewMock.LoggedInUser = userMock.Object;
             var svcMock = new Mock<IActivityService>();
             var testTarget = new ActivityListPresenter(viewMock, svcMock.Object);
             //Act
-
+            testTarget.InitView("");
             //Assert
+            Assert.True(viewMock.BeenRedirectedToLoginView);
+        }
 
+        [Fact]
+        public void LoggedInUserShouldOnlyBeDisplayedOwnActivitiesWhenSelectionIsBlank()
+        {
+            //Arrange
+            const int userId = 1;
+            var userMock = new Mock<IUserInfo>();
+            userMock.Setup(user => user.ID).Returns(userId);
+            var viewMock = new ActivityListViewMock();
+            viewMock.LoggedInUser = userMock.Object;
+            var svcMock = new Mock<IActivityService>();
+            var activities = GetTestActivities();
+            svcMock.Setup(ep => ep.GetByUserProfileId(userId)).Returns(GetTestActivities().Where(a => a.UserProfileId.Equals(1)).ToList());
+            var testTarget = new ActivityListPresenter(viewMock, svcMock.Object);
+            //Act
+            testTarget.InitView("");
+            //Assert
+            Assert.True(viewMock.DisplayedActivities.Count > 0);
+            foreach (var act in viewMock.DisplayedActivities)
+            {
+                Assert.Equal(1, act.UserProfileId);
+            }
+            Assert.Equal("Your own activities", viewMock.ListHeader);
         }
 
 
 
 
+        private IList<IActivity> GetTestActivities() 
+        {
+            var activities = new List<IActivity>();
+            for (int i = 1; i <= 5; i++)
+            {
+                var act = new ActivityMock { 
+                    UserProfileId = i, 
+                    Duration = 30 + i,
+                    Score = 5 + i,
+                    Created = DateTime.Now.AddDays(-i) 
+                };
+                activities.Add(act);
+            }
+            return activities;
+        }
 
-
-
-
+        internal class ActivityMock : IActivity
+        {
+            public int UserProfileId { get; set; }
+            public IUserInfo Performer { get; set; }
+            public DateTime ActivityDate { get; set; }
+            public int ActivityTypeId { get; set; }
+            public IActivityType ActivityType { get; set; }
+            public string OtherActivity { get; set; }
+            public int Duration { get; set; }
+            public int Score { get; set; }
+            public DateTime Created { get; set; }
+            public DateTime Updated { get; set; }
+        }
 
 
         internal class ActivityListViewMock : IActivityListView
         {
-            public IUserInfo LoggedInUser { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-            public string ListHeader { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+            private IUserInfo _userInfo;
+            private string _listHeader;
+            public bool BeenRedirectedToLoginView { get; private set; } = false;
+            public IList<IActivity> DisplayedActivities { get; private set; }
+            public string DisplayedErrorMessage { get; private set; }
+            public string DisplayedInfoMessage { get; private set; }
+
+
+            public IUserInfo LoggedInUser { get => _userInfo; set { _userInfo = value; } }
+            public string ListHeader { get => _listHeader; set { _listHeader = value; } }
 
             public void DisplayActivities(IList<IActivity> activities)
             {
-                throw new System.NotImplementedException();
+                DisplayedActivities = activities;
             }
 
             public void DisplayErrorMessage(string msg)
             {
-                throw new System.NotImplementedException();
+                DisplayedErrorMessage = msg;
             }
 
             public void DisplayInfoMessage(string msg)
             {
-                throw new System.NotImplementedException();
+                DisplayedInfoMessage = msg;
             }
 
             public void RedirectToLoginView()
             {
-                throw new System.NotImplementedException();
+                BeenRedirectedToLoginView = true;
             }
         }
     }
