2934c0e64891203db1b75b35b988564c81289fca
Mattias Fält <mattias.falt@umu.se>
2021-12-01 10:56:50 +0100

    p

--- src/Demos.WebformsMVP.BusinessLogic/Constants.cs
+++ src/Demos.WebformsMVP.BusinessLogic/Constants.cs
@@ -2,11 +2,6 @@
 {
     public static class Constants
     {
-        /// <summary>
-        /// Name in app.config/web.config
-        /// </summary>
-        public const string CONNECTION_STRING = "name=WebformsMVPDemoEntities";
-
         public static string UserObjectKey
         {
             get { return "Demos.WebformsMVP.UserInfo"; }
